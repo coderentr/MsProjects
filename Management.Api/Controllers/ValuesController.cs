cf486852e891fc593b33ec9471eb4725b7e8036c
Mustafa EREN <mstafaerenn@gmail.com>
2021-11-11 23:22:54 +0300

    api gateway

---
+++ Management.Api/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Management.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("Controle")]
+        public IActionResult GetManagementApi()
+        {
+            return Ok("management api is run");
+        }
+    }
+}
